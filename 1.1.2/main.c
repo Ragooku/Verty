3876e390ff241ba22b78595d9b292f408d9ccad6
Vladislav <64515222+Ragooku@users.noreply.github.com>
2025-09-27 10:08:39 +0400

    Add files via upload

---
+++ 1.1.2/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define LED GPIO_NUM_13 // указываем номер GPIO к которому подключен светодиод
+
+void app_main(void)
+{
+  gpio_set_direction(LED, GPIO_MODE_OUTPUT); // указываем режим работы GPIO
+  uint32_t led_on = 0;
+  while (1)
+  {
+    led_on = !led_on;
+    gpio_set_level(LED, led_on); // устанавливаем значение на выход GPIO: hight или low (0 или 1)
+    printf("Status - %ld \n", led_on);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
